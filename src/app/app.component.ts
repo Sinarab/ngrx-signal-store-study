ef1cdaba488168a1fb785e759e8db043e9fb2a62
SinaraB <sinara_b@outlook.com>
2024-03-13 10:39:08 -0300

    using RxJS Integration

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,13 +5,14 @@ import { JsonPipe } from '@angular/common';
 import { TodosListComponent } from './todos-list/todos-list.component';
 import { MatProgressSpinner, MatSpinner } from '@angular/material/progress-spinner';
 import { UserDashboardComponent } from './user-dashboard/user-dashboard.component';
+import { InfoComponent } from './info/info.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [
     RouterOutlet, JsonPipe, TodosListComponent,
-    MatProgressSpinner, UserDashboardComponent
+    MatProgressSpinner, UserDashboardComponent, InfoComponent
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
@@ -21,7 +22,7 @@ export class AppComponent implements OnInit {
 
   ngOnInit() {
     this.loadTodos()
-      .then(resp => console.log('Todos loaded', resp));
+      .then(() => console.log('Todos loaded'));
   }
 
   async loadTodos() {
---
+++ src/app/info/info.component.ts
@@ -0,0 +1,72 @@
+import { Component, signal } from '@angular/core';
+import { interval, map, pipe, tap } from 'rxjs';
+import { rxMethod } from '@ngrx/signals/rxjs-interop';
+import { MatFormField, MatLabel, MatSuffix } from '@angular/material/form-field';
+import { MatInput } from '@angular/material/input';
+import { MatIcon } from '@angular/material/icon';
+import { FormsModule } from '@angular/forms';
+import { MatButton, MatButtonModule } from '@angular/material/button';
+
+@Component({
+  selector: 'info',
+  standalone: true,
+  imports: [
+    MatFormField, MatLabel, MatInput,
+    MatIcon, FormsModule, MatButtonModule, MatSuffix
+  ],
+  templateUrl: './info.component.html',
+  styleUrl: './info.component.scss'
+})
+export class InfoComponent {
+
+  counterLabel = signal('Current second: 0');
+  readonly counter = rxMethod(
+    pipe(
+      map((y: number) => `Current second: ${y + 1}`),
+      tap((y: string) => this.counterLabel.set(y))
+    )
+  )
+
+  counter2Label = signal('Current second: 0');
+  readonly counter2 = rxMethod(
+    pipe(
+      map((y: number) => `Stop when reach 10s: ${y + 1}`),
+      tap((y: string) => this.counter2Label.set(y))
+    )
+  )
+
+  currentValue = signal(0);
+  result = signal(0);
+  value = '';
+  readonly calcPow = rxMethod(
+    pipe(
+      map((x: number) => x * x),
+      tap((x: number) => this.result.set(x))
+    )
+  );
+
+  constructor() {
+    // Each invocation of the reactive method pushes
+    // the input value through the reactive chain. When called
+    // with a static value, the reactive chain executes once.
+    this.calcPow(this.currentValue);
+
+    // When a reactive method is called with an observable,
+    // the reactive chain is executed every time the observable
+    // emits a new value.
+    const interval$ = interval(1000);
+    this.counter(interval$);
+    this.counter2(interval$);
+
+    // If a reactive method needs to be cleaned up before the injector
+    // is destroyed, manual cleanup can be performed by calling the unsubscribe method.
+    setTimeout(() => {
+      // 👇 Clean up the second reactive method subscription after 10 seconds.
+      this.counter2.unsubscribe();
+    }, 10000);
+  }
+
+  onCalcPow() {
+    this.currentValue.set(+this.value);
+  }
+}
